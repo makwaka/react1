51502bb2dc1c365cb0dd95b5d76f28f713c92429
Phariq <pharisnjuguna22@gmail.com>
2023-02-28 22:21:42 +0300

    learnt about props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/greet'
+import Welcome from './components/welcome';
+import Hello from './components/Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return(
+        <div className='App'>
+
+          <Greet name='Joy' heroName="batman">
+            <p>This is children props</p>
+          </Greet>
+          <Greet name='Mary' heroName="spider man">
+            <button>Action</button>
+          </Greet>
+          <Greet name='Ken' heroName="ant man"/>
+
+        <Welcome  name='Joy' heroName="batman"/>
+        <Welcome name='Mary' heroName="spider man"/>
+        <Welcome name='Ken' heroName="ant man"/>
+        </div>
+      )
+    }
 }
 
 export default App;
